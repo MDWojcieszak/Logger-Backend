a0adf7a14d5e2d18633366f98f522d7ffffc5412
MDWojcieszak <wojcieszak.4@gmail.com>
2024-06-03 19:32:03 +0200

    feature(prisma): module setup

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('DB Connected');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('DB Disconnected');
+  }
+}
